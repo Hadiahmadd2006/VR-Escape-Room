b8631ccc174eb20c59fd0ea977b0654269872517
Hadiahmadd2006 <ghareeb.hadi1@gmail.com>
2026-03-23 17:12:41 +0200

    Added finished puzzle sound, fixed second door animation
    When the player finishes a puzzle a sound is played and fixed the second door

--- Assets/Scripts/CardSocketDoor.cs
+++ Assets/Scripts/CardSocketDoor.cs
@@ -5,10 +5,14 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class CardSocketDoor : MonoBehaviour
 {
     private XRSocketInteractor _socketInteractor;
-
+    
     [Header("Door Settings")]
     [SerializeField] private Animator doorAnimator;
-
+    [SerializeField] private int puzzleNumberToSolve = 1; 
+    
+    [Header("Targets")]
+    [SerializeField] private GameObject blockerToRemove; 
+    
     [Header("Audio Settings")]
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip cardInsertSound;
@@ -16,30 +20,59 @@ public class CardSocketDoor : MonoBehaviour
     private void Awake()
     {
         _socketInteractor = GetComponent<XRSocketInteractor>();
+        
+        if (_socketInteractor == null)
+        {
+            Debug.LogError("CardSocketDoor script is missing an XRSocketInteractor on the same GameObject!", this);
+        }
     }
 
     private void OnEnable()
     {
-        _socketInteractor.selectEntered.AddListener(OnCardInserted);
+        if (_socketInteractor != null)
+        {
+            _socketInteractor.selectEntered.AddListener(OnCardInserted);
+        }
     }
 
     private void OnDisable()
     {
-        _socketInteractor.selectEntered.RemoveListener(OnCardInserted);
+        if (_socketInteractor != null)
+        {
+            _socketInteractor.selectEntered.RemoveListener(OnCardInserted);
+        }
     }
 
     private void OnCardInserted(SelectEnterEventArgs args)
     {
         if (audioSource != null && cardInsertSound != null)
-        {
             audioSource.PlayOneShot(cardInsertSound);
+            
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetBool("IsIdle", false);
+            doorAnimator.SetTrigger("Open");
         }
 
-        doorAnimator.SetBool("IsIdle", false);
-        doorAnimator.SetTrigger("Open");
-
-        Debug.Log("Keycard inserted! Door opening...");
-
-        _socketInteractor.selectEntered.RemoveListener(OnCardInserted);
+        if (blockerToRemove != null)
+        {
+            blockerToRemove.SetActive(false);
+        }
+        
+        Debug.Log($"Keycard inserted! Puzzle {puzzleNumberToSolve} solved.");
+        
+        if (GameProgressManager.Instance != null)
+        {
+            GameProgressManager.Instance.SolvePuzzle(puzzleNumberToSolve);
+        }
+        else
+        {
+            Debug.LogError("Wait! There is no GameProgressManager in the scene to record this puzzle!");
+        }
+        
+        if (_socketInteractor != null)
+        {
+            _socketInteractor.selectEntered.RemoveListener(OnCardInserted);
+        }
     }
 }
