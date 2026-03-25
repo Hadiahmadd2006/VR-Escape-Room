f4e44ebab34eab02388a07f96f2873e2cc40dce4
Hadiahmadd2006 <ghareeb.hadi1@gmail.com>
2026-03-25 19:28:31 +0200

    Color sequence script open door animator bug fix

--- Assets/Scripts/ColorSequencePuzzle.cs
+++ Assets/Scripts/ColorSequencePuzzle.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
@@ -14,7 +15,7 @@ public class ColorSequencePuzzle : MonoBehaviour
     [SerializeField] private int[] correctSequence = { 0, 2, 1, 3, 0 };
 
     [Header("Targets")]
-    [SerializeField] private Animator doorAnimator; 
+    [SerializeField] private Animator doorAnimator;
 
     [Header("Audio")]
     [SerializeField] private AudioSource audioSource;
@@ -28,68 +29,56 @@ public class ColorSequencePuzzle : MonoBehaviour
     private int _currentStep;
     private bool _solved;
 
+    private UnityAction<SelectEnterEventArgs> _redListener;
+    private UnityAction<SelectEnterEventArgs> _blueListener;
+    private UnityAction<SelectEnterEventArgs> _greenListener;
+    private UnityAction<SelectEnterEventArgs> _yellowListener;
+
     private void Awake()
     {
         _currentStep = 0;
+        _solved = false;
         if (successUI != null) successUI.SetActive(false);
+        _redListener = (args) => OnColorPressed(0);
+        _blueListener = (args) => OnColorPressed(1);
+        _greenListener = (args) => OnColorPressed(2);
+        _yellowListener = (args) => OnColorPressed(3);
     }
 
     private void OnEnable()
     {
-        if (redButton != null) 
-        {
-            redButton.selectEntered.RemoveAllListeners();
-            redButton.selectEntered.AddListener((a) => OnColorPressed(0));
-        }
-        if (blueButton != null) 
-        {
-            blueButton.selectEntered.RemoveAllListeners();
-            blueButton.selectEntered.AddListener((a) => OnColorPressed(1));
-        }
-        if (greenButton != null) 
-        {
-            greenButton.selectEntered.RemoveAllListeners();
-            greenButton.selectEntered.AddListener((a) => OnColorPressed(2));
-        }
-        if (yellowButton != null) 
-        {
-            yellowButton.selectEntered.RemoveAllListeners();
-            yellowButton.selectEntered.AddListener((a) => OnColorPressed(3));
-        }
+        if (redButton != null) redButton.selectEntered.AddListener(_redListener);
+        if (blueButton != null) blueButton.selectEntered.AddListener(_blueListener);
+        if (greenButton != null) greenButton.selectEntered.AddListener(_greenListener);
+        if (yellowButton != null) yellowButton.selectEntered.AddListener(_yellowListener);
     }
 
     private void OnDisable()
     {
-        if (redButton != null) redButton.selectEntered.RemoveAllListeners();
-        if (blueButton != null) blueButton.selectEntered.RemoveAllListeners();
-        if (greenButton != null) greenButton.selectEntered.RemoveAllListeners();
-        if (yellowButton != null) yellowButton.selectEntered.RemoveAllListeners();
+        if (redButton != null) redButton.selectEntered.RemoveListener(_redListener);
+        if (blueButton != null) blueButton.selectEntered.RemoveListener(_blueListener);
+        if (greenButton != null) greenButton.selectEntered.RemoveListener(_greenListener);
+        if (yellowButton != null) yellowButton.selectEntered.RemoveListener(_yellowListener);
     }
 
     private void OnColorPressed(int colorIndex)
     {
         if (_solved) return;
-        
         Debug.Log($"Button {colorIndex} pressed. We are on step {_currentStep}.");
-        
-        if (audioSource != null && buttonSound != null) 
-            audioSource.PlayOneShot(buttonSound);
-        
+        if (audioSource != null && buttonSound != null) audioSource.PlayOneShot(buttonSound);
         if (correctSequence[_currentStep] == colorIndex)
         {
             _currentStep++;
             Debug.Log($"Correct! Moving to step {_currentStep}.");
-            
             if (_currentStep >= correctSequence.Length)
             {
                 Debug.Log("PUZZLE SOLVED! Opening Door.");
                 _solved = true;
-                
                 if (audioSource != null) audioSource.Stop();
                 if (audioSource != null && correctSound != null) audioSource.PlayOneShot(correctSound);
-                
                 if (doorAnimator != null)
                 {
+                    doorAnimator.SetBool("IsIdle", false);
                     doorAnimator.SetTrigger("Open");
                     Debug.Log("'Open' trigger sent to Door Animator.");
                 }
@@ -97,11 +86,8 @@ public class ColorSequencePuzzle : MonoBehaviour
                 {
                     Debug.LogWarning("ERROR: Door Animator is MISSING in the Inspector!");
                 }
-
                 if (successUI != null) successUI.SetActive(true);
-                
-                if (GameProgressManager.Instance != null)
-                    GameProgressManager.Instance.SolvePuzzle(3);
+                if (GameProgressManager.Instance != null) GameProgressManager.Instance.SolvePuzzle(3);
             }
         }
         else
